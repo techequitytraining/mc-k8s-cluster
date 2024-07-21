80b9f1ec0bf8ba73c37baf11680a67fd82a1df45
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2024-07-21 22:56:52 +0100

    Add option to switch between modes

--- gcp-gke-mc.sh
+++ gcp-gke-mc.sh
@@ -129,6 +129,7 @@ cat<<EOF
 Configure Kubernetes on AWS, Azure and GCP
 -----------------------------------------------
 Please enter number to select your choice:
+ (0) Switch between Preview, Execute and Delete modes
  (1) Set cloud platform
  (2) Download SDK
  (3) Authenticate
